83cc99949138bf0e75dd10ade6169784d365f08b
Agustri <agus.triyono@gmail.com>
2026-08-25 13:11:24 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,3 @@
 import './globals.css';
-export const metadata={title:'Era Super League | FPL',description:'Era Super League Fantasy Premier League dashboard'};
+export const metadata={title:'4G LTE Super League | FPL',description:'4G LTE Super League Fantasy Premier League dashboard'};
 export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="id"><body>{children}</body></html>}
